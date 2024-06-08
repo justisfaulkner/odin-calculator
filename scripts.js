59d41197a102f3c825aa021702954ab09edbe893
Justis Faulkner <faulknerjustis@gmail.com>
2024-06-08 15:29:36 -0400

    Finish adding calculator functionality

--- scripts.js
+++ scripts.js
@@ -7,10 +7,10 @@ let result = "";
 let operated = false;
 
 function operate(firstOperand, operator, secondOperand) {
-    const first = Number(firstOperand)
-    const second = Number(secondOperand)
+    const first = Number(firstOperand);
+    const second = Number(secondOperand);
     if (second === 0 && operator === "÷") {
-        return "don't divide by zero";
+        return "pls don't ÷ by 0, thx";
     }
     switch (operator) {
         case "÷":
@@ -29,21 +29,22 @@ function operate(firstOperand, operator, secondOperand) {
             result = "something went wrong";
             break;
     }
-    
     return round(result);
 }
 
 function round(num) {
-    return Math.round((num + Number.EPSILON) * 100) / 100;
+    return Math.round((num + Number.EPSILON) * 10000000) / 10000000;
 }
 
 const inputsContainer = document.querySelector(".inputs-container");
 const screenInput = document.querySelector(".screen-input");
 const screenResult = document.querySelector(".screen-result");
 
+// to do: add keyboard support
 inputsContainer.addEventListener("click", handleCalculatorBtnClick);
 
 export function handleCalculatorBtnClick(e) {
+    console.log(e)
     const buttonClicked = e.target.id;
     const innerText = e.target.innerText;
 
@@ -55,15 +56,23 @@ export function handleCalculatorBtnClick(e) {
     switch (buttonClicked) {
         case "":
             break;
+        // to do: if any operand is clicked twice, have the calculator operate
         case "divide":
+            if (operated) continueOperation();
             break;
         case "multiply":
+            if (operated) continueOperation();
             break;
         case "subtract":
+            if (operated) continueOperation();
             break;
         case "add":
+            if (operated) continueOperation();
             break;
         case "decimal":
+            if (operated) {
+                break;
+            }
             if (!operandClicked) {
                 if (!firstOperand.includes(".") && firstOperand !== "") {
                     firstOperand += innerText;
@@ -77,22 +86,64 @@ export function handleCalculatorBtnClick(e) {
             }
             break;
         case "clear":
-            firstOperand = "";
-            secondOperand = "";
-            operator = "";
-            operandClicked = false;
-            screenInput.innerHTML = "";
-            screenResult.innerHTML = "";
+            clear();
             break;
-        // case "delete"
+        // to do: add delete functionality
+        case "delete":
+            break;
         case "equals":
-            screenResult.innerHTML = operate(firstOperand, operator, secondOperand);
+            if (firstOperand === "" || secondOperand === "") break;
+            screenResult.innerText = operate(
+                firstOperand,
+                operator,
+                secondOperand
+            );
+            operated = true;
             break;
         default:
+            if (operated) clear();
             !operandClicked
                 ? (firstOperand += innerText)
                 : (secondOperand += innerText);
     }
-    screenInput.innerHTML =
+    screenInput.innerText =
         `${firstOperand} ${operator} ${secondOperand}`.trim();
+
+    let displayLength =
+        firstOperand.toString().length + secondOperand.toString().length;
+    if (displayLength >= 14 && displayLength < 19) {
+        screenInput.style.fontSize = "36px";
+        screenResult.style.fontSize = "36px";
+    } else if (displayLength >= 19 && displayLength < 27) {
+        screenInput.style.fontSize = "26px";
+        screenResult.style.fontSize = "26px";
+    } else if (displayLength >= 27 && displayLength < 45) {
+        screenInput.style.fontSize = "16px";
+        screenResult.style.fontSize = "16px";
+    } else if (displayLength >= 45) {
+        screenInput.style.fontSize = "8px";
+        screenResult.style.fontSize = "8px";
+    } else {
+        screenInput.style.fontSize = "46px";
+        screenResult.style.fontSize = "46px";
+    }
+}
+
+function clear() {
+    firstOperand = "";
+    secondOperand = "";
+    operator = "";
+    operandClicked = false;
+    screenInput.innerText = "";
+    screenResult.innerText = "";
+    operated = false;
+}
+
+function continueOperation() {
+    firstOperand = result;
+    secondOperand = "";
+    result = "";
+    screenResult.innerText = "";
+    operated = false;
+    operandClicked = true;
 }
